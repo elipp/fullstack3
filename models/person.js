d0bc8eca0e6e4bf294ed71cf65119394268a9038
Elias Miettinen <elias.miettinen@aalto.fi>
2021-03-04 01:46:45 +0200

    xd

---
+++ models/person.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[process.argv.length-1] || "NOPASSWORD"
+
+const url =`mongodb+srv://mongouser:${password}@cluster0.prh5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+console.log(`connecting to ${url}`)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+	id: Number,
+	name: String,
+	number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+personSchema.set('toJSON', {
+	  transform: (document, returnedObject) => {
+		      returnedObject.id = returnedObject._id.toString()
+		      delete returnedObject._id
+		      delete returnedObject.__v
+		    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
